92281181f843a7d28c74e154ac18ecd29337e8ab
mingyuesun <1014422582@qq.com>
2020-07-24 15:41:26 +0800

    Add make-redux folder

---
+++ make-redux/README.md
@@ -0,0 +1,26 @@
+## reducer
+
+```js
+// 定一个 reducer
+function reducer (state, action) {
+  /* 初始化 state 和 switch case */
+}
+
+// 定义一个 createStore
+function createStore(reducer){
+
+	return {getState, dispatch, subscribe}
+}
+
+// 生成 store
+const store = createStore(reducer)
+
+// 监听数据变化重新渲染页面
+store.subscribe(() => renderApp(store.getState()))
+
+// 首次渲染页面
+renderApp(store.getState()) 
+
+// 后面可以随意 dispatch 了，页面自动更新
+store.dispatch(...)
+```
---
+++ make-redux/src/index.js
@@ -0,0 +1,93 @@
+// const appState = {
+//   title: {
+//     text: 'React.js 小书',
+//     color: 'red',
+//   },
+//   content: {
+//     text: 'React.js 小书内容',
+//     color: 'blue'
+//   }
+// }
+
+function createStore(reducer){
+  let state = null
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = reducer(state, action) // 覆盖原来的对象
+    listeners.forEach(listener => listener())
+  }
+  dispatch({}) // 初始化 state
+  return { getState, dispatch, subscribe }
+}
+
+function stateChanger(state, action) {
+  if (!state) {
+    return {
+      title: {
+        text: 'React.js 小书',
+        color: 'red',
+      },
+      content: {
+        text: 'React.js 小书内容',
+        color: 'blue'
+      }
+    }
+  }
+  switch(action.type){
+    case 'UPDATE_TITLE_TEXT':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
+    case 'UPDATE_TITLE_COLOR':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
+    default:
+      return state    
+  }
+}
+
+function renderApp (newAppState, oldAppState = {}) {
+  if (newAppState === oldAppState) return
+  console.log('render app...')
+  renderTitle(newAppState.title, oldAppState.title)
+  renderContent(newAppState.content, oldAppState.content)
+}
+
+function renderTitle (newTitle, oldTitle = {}) {
+  if (newTitle === oldTitle) return
+  console.log('render title...')
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = newTitle.text
+  titleDOM.style.color = newTitle.color
+}
+
+function renderContent (newContent, oldContent = {}) {
+  if (newContent === oldContent) return
+  console.log('render content...')
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = newContent.text
+  contentDOM.style.color = newContent.color
+}
+
+const store = createStore(stateChanger)
+let oldState = store.getState()
+store.subscribe(() => {
+  const newState = store.getState()
+  renderApp(newState, oldState)
+  oldState = newState
+}) // 监听数据变化
+renderApp(store.getState()) // 首次渲染页面
+store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '《React.js 中文小书》'})
+store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'orange'})
+
